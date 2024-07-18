aef22f7f7026f7e0dd953632b8de863790ed2f40
FireStealerz <ivan.herran0@gmail.com>
2024-07-17 20:51:30 -0600

    fist commit

---
+++ compiladorELA.py
@@ -0,0 +1,56 @@
+'''
+compiladorELA.py
+
+Identificadores aceptados
+var = variable
+const = constante
+
+palabras reservadas
+var
+const
+int 
+float
+char
+string
+
+Tipos de datos
+integer
+float
+char
+string
+
+Operadores Aritmeticos
++
++
+*
+/
+
+Operadores Relacionales
+<
+>
+<=
+>=
+==
+
+Operadores Logicos
+or
+and
+true
+false
+
+Caracteres especiales 
+0
+; 
+{}
+()
+
+Sentencias Controladores
+IF()
+WHILE()
+DO - WHILE()
+FOR(int x; x <= 0; x++)
+'''
+
+
+
+fileELA = open("")
