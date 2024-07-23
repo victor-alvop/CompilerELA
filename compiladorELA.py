16b76729c091025436e9bdb226bbc8dd5ca7cfda
FireStealerz <ivan.herran0@gmail.com>
2024-07-23 14:13:13 -0600

    Added PLY dependency and initalized tokens
    Added the lex.py and yacc.py dependencies for lexical and sintactic analysis and parsing

--- compiladorELA.py
+++ compiladorELA.py
@@ -1,3 +1,6 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
 '''
 compiladorELA.py
 
@@ -53,4 +56,82 @@ FOR(int x; x <= 0; x++)
 
 
 
-fileELA = open("")
+tokens = (
+    #Palabras reservadas
+    'VAR'
+    'CONST'
+    'INT'
+    'FLOAT'
+    'CHAR'
+    'STRING'
+    #Operadores aritmeticos
+    'SUM'
+    'MINUS'
+    'DIV'
+    'MULT'
+    #Operadores logicos
+    'LESS'
+    'LESSEQ'
+    'EQ'
+    'MORE'
+    'MOREEQ'
+    'OR'
+    'AND'
+    #Booleanos
+    'TRUE'
+    'FALSE'
+    #Simbolos
+    'PARIZQ'
+    'PARDER'
+    'CORIZQ'
+    'CORDER'
+    'SEMICOL'
+    #Operadores
+    'IF'
+    'WHILE'
+    'DO'
+    'FOR'
+    #Asignar
+    'ASIGN'
+)
+
+t_VAR = r'\bvar\b'
+t_CONST = r'\bconst\b'
+t_INT = r'\bint\b'
+t_FLOAT = r'\bfloat\b'
+t_CHAR = r'\bchar\b'
+t_STRING = r'\bstring\b'
+t_SUM = r'\+'
+t_MINUS = r'\-'
+t_DIV = r'/'
+t_MULT = r'\*'
+t_LESS = r'<'
+t_LESSEQ = r'<='
+t_EQ = r'=='
+t_MORE = r'>'
+t_MOREEQ = r'>='
+t_OR = r'\|\|'
+t_AND = r'\&\&'
+t_PARIZQ = r'\('
+t_PARDER = r'\)'
+t_CORIZQ = r'\{'
+t_CORDER = r'\}'
+t_SEMICOL = r';'
+t_ASIGN = r'='
+
+
+def t_VAR(t):
+    r'\bvar'
+    return t;
+
+def t_CONST(t):
+    r'const'
+    return t
+
+def t_INT(t):
+    r''
+    t.value = int(t.value)
+    return t;
+
+def t_FLOAT(t):
+    
